7dacd7a03194fb09ccce47e5b475a671f65c4329
Taksh9347 <i24ai023@aid.svnit.ac.in>
2025-03-11 10:34:41 +0530

    UPLOAD

--- LAB_7_8/8.py
+++ LAB_7_8/8.py
@@ -1,3 +1,5 @@
+# 122016
+# [1,1,2,3,2,2,4]
 def decode(code):
     n = len(code)
     ways_to_decode = [0]*(n+1) # create list to store ways to decade strings [0,0,0....n+1 times]
@@ -13,6 +15,7 @@ def decode(code):
                 ways_to_decode[i] += ways_to_decode[i-1]
             if(two_digit>=10 and two_digit<=26):
                 ways_to_decode[i] += ways_to_decode[i-2] 
+    # print(ways_to_decode)
     return ways_to_decode[n]
 str = input("Enter encoded message : ")
 print(decode(str))
