03cf4cfd9c77c21dcb5a223687c48d3226f6e1fa
Taksh9347 <i24ai023@aid.svnit.ac.in>
2025-04-15 11:10:23 +0530

    UPLOAD

--- LAB_11/4.py
+++ LAB_11/4.py
@@ -6,7 +6,7 @@ party = pd.DataFrame({'days':range(1,11),
 can_party = [i for i in range(10) if party['John'][i]=='Yes' and party['Judy'][i]=='Yes']
 days=[]
 for i in range(10):
-    future_parties = [day - i for day in can_party if day >= i]
+    future_parties = [day - i for day in can_party if day >= i] # list of diff bw future days
     days.append(min(future_parties) if future_parties else None)
-party['days_till_party'] = days
+party['days_till_party'] = days # add new column of days till party
 print(party)
--- LAB_11/5.py
+++ LAB_11/5.py
@@ -1,18 +1,22 @@
 import pandas as pd
-concert = pd.DataFrame({'artist':['A','C','B','A','B','C','A'],
-                        'Venue':['v1','v2','v3','v1','v2','v3','v2'],
-                        'Year-Month':['2020-03','2021-04','2023-05','2023-07','2023-05','2020-03','2020-03']})
-# get unique series of artists and venues
-arti = pd.Series(sorted(list(set(concert['artist']))),name='artist')
-ven = pd.Series(sorted(list(set(concert['Venue']))),name='Venue')
-# counting the number of concerts for the pair (artist,venue)
-counts = concert.groupby(['artist','Venue','Year-Month']).size().reset_index(name='Counts')
-# create a wide table for output
+import itertools
+concert = pd.DataFrame({
+    'artist': ['A', 'C', 'B', 'A', 'B', 'C', 'A'],
+    'Venue': ['v1', 'v2', 'v3', 'v1', 'v2', 'v3', 'v2'],
+    'Year-Month': ['2020-03', '2021-04', '2023-05', '2020-03', '2023-05', '2020-03', '2020-03']
+})
+arti = pd.Series(sorted(concert['artist'].unique()), name='artist')
+ven = pd.Series(sorted(concert['Venue'].unique()), name='Venue')
 wide = pd.pivot_table(concert,
-                      columns=['artist','Venue'],
+                      columns=['artist', 'Venue'],
                       index='Year-Month',
                       aggfunc='size',
                       fill_value=0)
-# Flatten Column multi-index -> i.e (artist,value) pairs
-wide.columns = [f'({a},{v})' for a, v in wide.columns]
+# Create all combinations
+all_comb = list(itertools.product(arti, ven)) # cross product 
+for a, v in all_comb:
+    if (a, v) not in wide.columns:
+        wide[(a, v)] = 0  # if a col is missing then make it zero
+wide = wide.reindex(sorted(wide.columns), axis=1) # give proper index after editing wide.columns
+wide.columns = [f'({a},{v})' for a, v in wide.columns] # convert ('A','v1') -> (A,v1)
 print(wide)
